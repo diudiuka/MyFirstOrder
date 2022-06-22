91cebd29d47d2a82093421fef457035a38f4a6c6
evgeny.kamiak <evgeny.kamiak@hiqo-solutions.com>
2022-06-22 21:20:03 +0300

    Add project files.

---
+++ MyFirstOrder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyFirstOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[] { 99, 98, 92, 97,98,456,86 };
+            marks = Sort(marks);
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine(marks[i]);
+            }
+            
+        }
+        static int[] Sort(int[] a)
+        {
+
+            return a;
+
+        }
+
+
+
+
+    }
+}
