87c703114de5948552472461024e1e9bf0fc6510
evgeny.kamiak <evgeny.kamiak@hiqo-solutions.com>
2022-06-26 19:57:53 +0300

    second step

--- MyFirstOrder/Program.cs
+++ MyFirstOrder/Program.cs
@@ -6,18 +6,29 @@ namespace MyFirstOrder
     {
         static void Main(string[] args)
         {
-            int[] marks = new int[] { 99, 98, 92, 97,98,456,86 };
+            int[] marks = new int[] { 99, 98, 92, 97, 98, 456, 86 };
             marks = Sort(marks);
             for (int i = 0; i < marks.Length; i++)
             {
                 Console.WriteLine(marks[i]);
             }
-            
+
         }
         static int[] Sort(int[] a)
         {
+            int[] result = new int[a.Length];
 
-            return a;
+            int min = int.MaxValue;
+            for (int i = 0; i < a.Length; i++) 
+            {
+                if (a[i] < min)
+                {
+                    min = a[i];
+                }
+            }
+
+
+            return result;
 
         }
 
