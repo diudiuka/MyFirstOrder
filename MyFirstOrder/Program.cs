13aa23f0c02130b2ac7d3aa290e2124d2f8d474c
evgeny.kamiak <evgeny.kamiak@hiqo-solutions.com>
2022-06-30 22:02:36 +0300

    stage 4

--- MyFirstOrder/Program.cs
+++ MyFirstOrder/Program.cs
@@ -17,17 +17,17 @@ namespace MyFirstOrder
         static int[] Sort(int[] a)
         {
             int[] result = new int[a.Length];
-            int mim5 = FindMin(a);
+            int mim5 = FindMinButMoreThan(a, 0);
             Console.WriteLine("min: " + mim5);
             return result;
         }
 
-        static int FindMin(int[] array)
+        static int FindMinButMoreThan(int[] array, int intAfter)
         {
             int min = int.MaxValue;
             for (int i = 0; i < array.Length; i++)
             {
-                if (array[i] < min)
+                if (array[i] < min && array[i] > intAfter)
                 {
                     min = array[i];
                 }
